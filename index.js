d74b22ef025a4443a3512451bf8ab8ac14ee6035
Jorge Jimenez <calenocam24@gmail.com>
2020-10-08 15:49:09 -0400

    fixing error again

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const server = require("./server.js");
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
   console.log(`\n*** Server running at http://localhost:${port} ***\n`);
