6e83f867aa0d4081368f0895f8f6f6adb58b27b1
Jorge Jimenez <calenocam24@gmail.com>
2020-10-08 15:30:01 -0400

    better code with async

--- users/userRouter.js
+++ users/userRouter.js
@@ -4,40 +4,44 @@ const postModel = require("../posts/postDb");
 
 const router = express.Router();
 
-router.post("/", validateUser(), (req, res) => {
+router.post("/", validateUser(), async (req, res) => {
   // do your magic!
-  userModel
-    .insert(req.body)
-    .then((post) => {
-      res.status(201).json(post);
-    })
-    .catch((error) => {
-      next(error);
-    }); // a shorter way to call next with the error
+  try {
+    const user = await userModel.insert(req.body);
+
+    res.status(201).json(user);
+  } catch (error) {
+    next(error);
+  } // a shorter way to call next with the error
 });
 
-router.post("/:id/posts", validatePost(), validateUserId(), (req, res) => {
-  // do your magic!
-  postModel
-    .insert({ text: req.body.text, user_id: req.params.id })
-    .then((post) => {
-      res.status(201).json(post);
-    })
-    .catch((error) => {
-      next(error);
-    });
-});
+router.post(
+  "/:id/posts",
+  validatePost(),
+  validateUserId(),
+  async (req, res) => {
+    // do your magic!
+    try {
+      const user = await postModel.insert({
+        text: req.body.text,
+        user_id: req.params.id,
+      });
 
-router.get("/", (req, res) => {
-  // do your magic!
-  userModel
-    .get()
-    .then((users) => {
-      res.status(200).json(users);
-    })
-    .catch((error) => {
+      res.status(201).json(user);
+    } catch (error) {
       next(error);
-    });
+    }
+  }
+);
+
+router.get("/", async (req, res) => {
+  // do your magic!
+  try {
+    const user = await userModel.get();
+    res.status(200).json(user);
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.get("/:id", validateUserId(), (req, res) => {
@@ -45,79 +49,74 @@ router.get("/:id", validateUserId(), (req, res) => {
   res.status(200).json(req.user);
 });
 
-router.get("/:id/posts", validateUserId(), (req, res) => {
+router.get("/:id/posts", validateUserId(), async (req, res) => {
   // do your magic!
-  userModel
-    .getUserPosts(req.params.id)
-    .then((posts) => {
-      res.status(200).json(posts);
-    })
-    .catch((error) => {
-      next(error);
-    });
+  try {
+    const user = await userModel.getUserPosts(req.params.id);
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.delete("/:id", validateUserId(), (req, res) => {
+router.delete("/:id", validateUserId(), async (req, res) => {
   // do your magic!
-  userModel
-    .remove(req.params.id)
-    .then((count) => {
-      if (count > 0) {
-        res.status(200).json({
-          message: "The user has been nuked",
-        });
-      } else {
-        res.status(404).json({
-          message: "The user could not be found",
-        });
-      }
-    })
-    .catch((error) => {
-      next(error);
-    });
+  try {
+    const user = await userModel.remove(req.params.id);
+
+    if (user > 0) {
+      res.status(200).json({
+        message: "The user has been nuked",
+      });
+    } else {
+      res.status(404).json({
+        message: "The user could not be found",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.put("/:id", validateUser(), validateUserId(), (req, res) => {
+router.put("/:id", validateUser(), validateUserId(), async (req, res) => {
   // do your magic!
-  userModel
-    .update(req.params.id, req.body)
-    .then((user) => {
-      if (user) {
-        res.status(200).json(user);
-      } else {
-        res.status(404).json({
-          message: "The user could not be found",
-        });
-      }
-    })
-    .catch((error) => {
-      next(error);
-    });
+  try {
+    const user = await userModel.update(req.params.id, req.body);
+
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({
+        message: "The user could not be found",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
 //custom middleware
 
 function validateUserId() {
   // do your magic!
-  return (req, res, next) => {
-    userModel
-      .getById(req.params.id)
-      .then((user) => {
-        if (user) {
-          req.user = user;
-          next();
-        } else {
-          res.status(400).json({
-            message: "Invalid user id",
-          });
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-        res.status(500).json({
-          message: "Error retrieving the user",
+  return async (req, res, next) => {
+    try {
+      const user = await userModel.getById(req.params.id);
+
+      if (user) {
+        req.user = user;
+        next();
+      } else {
+        res.status(400).json({
+          message: "Invalid user id",
         });
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        message: "Error retrieving the user",
       });
+    }
   };
 }
 
